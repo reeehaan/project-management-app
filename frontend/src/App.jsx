f130d3ab8e61a943ec016ab3a54727befdca77a9
reeehaan <rehan221104@gmail.com>
2025-10-16 00:32:26 +0530

    feat: Design a sidebar to add new projects with a button

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import ProjectSidebar from './components/ProjectSidebar.jsx';
+
+function App() {
+  return (
+    <main className='h-screen my-8'>
+      <ProjectSidebar/>
+
+    </main>
+  );
+}
+
+export default App;
