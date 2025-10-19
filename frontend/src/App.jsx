e55aa60db1b757516289bd5342f35d105cfddc99
reeehaan <rehan221104@gmail.com>
2025-10-19 21:26:46 +0530

    feat: implement the add new project section with conditional component rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+
+import NewProject from './components/NewProject.jsx';
+import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectSidebar from './components/ProjectSidebar.jsx';
 
 function App() {
-  return (
-    <main className='h-screen my-8'>
-      <ProjectSidebar/>
+  const [projectState, setProjectState] = useState({
+    selectedProjectId : undefined,
+    projects: []
+  });
 
+  function handleStartAddProject(){
+    setProjectState(prevState =>{
+      return{
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  let content;
+
+  if(projectState.selectedProjectId === null){
+    content = <NewProject/>
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+  return (
+    <main className='h-screen my-8 flex gap-08'>
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
