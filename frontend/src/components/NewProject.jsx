ab8051f70406118f67b1d5dc32998b9c27da06d7
reeehaan <rehan221104@gmail.com>
2025-10-24 01:11:39 +0530

    feat: show modal pop up for invalid user inputs and add the project cancel button function

--- frontend/src/components/NewProject.jsx
+++ frontend/src/components/NewProject.jsx
@@ -1,7 +1,11 @@
 import Input from "./Input";
 import {useRef} from "react";
+import Modal from "./Modal";
 
-export default function NewProject({onAdd}){
+export default function NewProject({onAdd ,onCancel}){
+
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,6 +15,11 @@ export default function NewProject({onAdd}){
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === ''){
+            //show the error modal
+            modal.current.open();
+            return;
+        }
         onAdd({
             title : enteredTitle,
             description : enteredDescription,
@@ -18,9 +27,15 @@ export default function NewProject({onAdd}){
         })
     }
     return(
+        <>
+        <Modal ref={modal} buttonCaption='Okay'>
+            <h2 className="text-xl font-bold text-stone-700 mt-4 my-4">Invalid Input</h2>
+            <p className="text-stone-600 mb-4">oops... looks like you forgot to enter a value.</p>
+            <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
+        </Modal>
         <div className="w-[35rem] mt-16 px-8">
             <menu className="flex items-center justify-end gap-4 my-4">
-                <li><button className="text-stone-800 hover:text-stone-900">Cancel</button></li>
+                <li><button className="text-stone-800 hover:text-stone-900" onClick={onCancel}>Cancel</button></li>
                 <li><button 
                 onClick={handleSave}
                 className="px-6 py-2 rounded-md bg-stone-900 text-stone-50 hover:bg-stone-950">Save</button></li>
@@ -42,5 +57,6 @@ export default function NewProject({onAdd}){
                 />
             </div>
         </div>
+        </>
     );
 }
