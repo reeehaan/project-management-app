5b7b64093e291851f21d96661003bc3f08f7c657
reeehaan <rehan221104@gmail.com>
2025-10-26 20:58:53 +0530

    fix: add selectedProjectId prop to highlight selected project in sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NewProject from './components/NewProject.jsx';
 import NoProjectSelected from './components/NoProjectSelected.jsx';
 import ProjectSidebar from './components/ProjectSidebar.jsx';
+import SelectedProject from './components/SelectedProject.jsx';
 
 function App() {
   const [projectState, setProjectState] = useState({
@@ -9,6 +10,14 @@ function App() {
     projects: []
   });
 
+  function handleSelectProject(id){
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      }
+    }); 
+  }
   function handleStartAddProject(){
     setProjectState(prevState => {
       return {
@@ -41,8 +50,19 @@ function App() {
       }
     });
   }
+  function handleDeleteProject(){
+     setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project)=> project.id !== prevState.selectedProjectId),
+      };
+    }); 
+  }
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
 
-  let content;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+
   if(projectState.selectedProjectId === null){
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
   } else if(projectState.selectedProjectId === undefined){
@@ -54,6 +74,8 @@ function App() {
       <ProjectSidebar 
         onStartAddProject={handleStartAddProject}
         projects={projectState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectState.selectedProjectId}
       />
       {content}
     </main>
